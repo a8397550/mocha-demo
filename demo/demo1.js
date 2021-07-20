2e62aa01a2a28788153b1847e4a8f58545dd6c50
SUPCON\lijunyang <lijunyang@supcon.com>
2021-07-20 09:43:12 +0800

    demo

--- demo/demo1.js
+++ demo/demo1.js
@@ -9,4 +9,14 @@ describe('加法函数的测试', function() {
   it('1 加 1 应该等于 2', function() {
     expect(add(1, 1)).to.be.equal(2);
   });
+
+  // 跳过这条
+  it.skip('1 加 1 应该等于 2', function() {
+    expect(add(1, 1)).to.be.equal(2);
+  });
+
+  // 只执行这条
+  it.only('1 加 1 应该等于 2', function() {
+    expect(add(1, 1)).to.be.equal(2);
+  });
 });
